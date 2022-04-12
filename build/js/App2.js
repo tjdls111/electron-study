5faeed6902f5501afff44bdb15830e44fcb935c3
Seoin <tjdls111@naver.com>
2022-04-12 15:24:20 +0900

    npx create-electron-app 으로 프로젝트 다시 생성

--- build/js/App2.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function App2() {
-  return (
-    <div>App2</div>
-  )
-}
-
-export default App2
--- src/js/index.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import ReactDom from "react-dom";
-import App2 from "../../build/js/App2";
-
-const domContainer = document.querySelector("#root");
-ReactDom.render(<App2 />, domContainer);
